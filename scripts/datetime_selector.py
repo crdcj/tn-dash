74517bc166c19ca34f3ab0e495c241bc8c65a7e5
Felipe <felipecosta8449@hotmail.com>
2024-08-18 23:25:47 -0300

    final commit

--- scripts/datetime_selector.py
+++ scripts/datetime_selector.py
@@ -7,9 +7,9 @@ def dt_selector():
     end_date = datetime.now()
     input_date = pyd.bday.offset(dates=end_date, offset=-7)
 
-    titulo_list = ["LTN", "NTN-F", "NTN-B", "LFT"]
+    titulo_list = ["LTN", "NTN-F", "NTN-B", "LFT", 'DI']
 
-    st.session_state.columns_datetime = st.columns(8)
+    st.session_state.columns_datetime = st.columns([2,2,2,1,2,3,8])
 
     st.session_state.start_date = (
         st.session_state.columns_datetime[0].date_input("Data de Início", input_date).strftime("%Y-%m-%d")
@@ -20,6 +20,15 @@ def dt_selector():
     )
 
     st.session_state.titulo = (
-        st.session_state.columns_datetime[2].selectbox("Título", titulo_list)
+        st.session_state.columns_datetime[2].selectbox("Instrumento", titulo_list)
     )
+
+    st.session_state.toggle_taxa_1 = st.session_state.columns_datetime[4].toggle('taxa')
+
+    if st.session_state.titulo == "NTN-B":
+        st.session_state.on = st.session_state.columns_datetime[5].toggle('taxas $< 6$ meses')
+
+    elif st.session_state.titulo == 'NTN-F':
+        if not st.session_state.toggle_taxa_1:
+            st.session_state.toggle_premio_limpo = st.session_state.columns_datetime[5].toggle('prêmio limpo')
 
